ec42f79b1aa806c1c7dec01c222e69f58b788d74
Phuoc Nguyen <daiphuoc1914@gmail.com>
2026-03-22 18:48:44 +0700

    delete app folder

--- app/utils/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-from app.core.config import settings
-
-logging.basicConfig(
-    level=getattr(logging, settings.log_level.upper()),
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger("ai-service")
